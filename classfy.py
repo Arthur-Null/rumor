1d19d21850ed819645b6d826aae667e7c64189c5
jz <6140976372@qq.com>
2018-05-30 21:57:40 +0800

    add classfy

---
+++ classfy.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+path = 'dataset/'         # 替换为你的路径\
+traindir = 'train/'
+testdir =  'test/'
+
+dir = os.listdir(path)
+one = 0
+zero =0
+for i in dir:
+    if i[-5]=='1':
+        one+=1
+    else:
+        zero+=1
+print(one,zero)
+
+train_one = 400
+train_zero =400
+
+zero_num  = 0
+one_num = 0
+for file in os.listdir(path):
+    srcFile = os.path.join(path,file)
+    if file[-5] == '1':
+        if one_num < train_one:
+            targetFile = os.path.join(traindir,file)
+            shutil.copyfile(srcFile,targetFile)
+            one_num+=1
+        else:
+            targetFile = os.path.join(testdir, file)
+            shutil.copyfile(srcFile, targetFile)
+    else:
+        if zero_num < train_zero:
+            targetFile = os.path.join(traindir, file)
+            shutil.copyfile(srcFile, targetFile)
+            zero_num+=1
+        else:
+            targetFile = os.path.join(testdir, file)
+            shutil.copyfile(srcFile, targetFile)
